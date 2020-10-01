dbc9d82c204203ed1a9db1bf0cae22d653fb97a1
pajid9763 <pajid9763@wrdsb.ca>
2020-10-01 18:14:25 +0000

    finished program that tells a number's written form

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program to write the written form of a number between 1-10
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,47 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  Scanner input = new Scanner(System.in);
+
+  // ask the user for a positive integer
+  System.out.println("Please enter a positive integer:");
+  int num = input.nextInt();
+
+  switch (num){
+    case 1:
+      System.out.println("one");
+      break;
+    case 2:
+      System.out.println("two");
+      break;
+    case 3:
+      System.out.println("three");
+      break;
+    case 4:
+      System.out.println("four");
+      break;
+    case 5:
+      System.out.println("five");
+      break;
+    case 6:
+      System.out.println("six");
+      break;
+    case 7:
+      System.out.println("seven");
+      break;
+    case 8:
+      System.out.println("eight");
+      break;
+    case 9:
+      System.out.println("nine");
+      break;
+    case 10:
+      System.out.println("ten");
+      break;
+    default:
+      System.out.println("not in the range");
+      break;
+
+  }
   }
 }
